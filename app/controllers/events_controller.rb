4455ed3b6a01c83314c4164484857048ad3aa388
Dave Paola <dpaola2@gmail.com>
2014-04-09 20:08:06 -0700

    added event count

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   before_filter :authenticate_user!, :except => [:create]
   
   def index
+    @event_count = Event.count
     @events = Event.order('created_at DESC').page(params[:page]).per_page(100)
   end
 
