ff3c6b38922566f52bd9c4c4b63b2cd0398c0d4c
Doug Wright <mrdougwright@gmail.com>
2014-04-13 14:20:15 -0700

    Update FactoryGirl event to correct mock. Create some testing around Standard Event Spec

--- spec/models/standard_event_spec.rb
+++ spec/models/standard_event_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe StandardEvent do
+  before do
+    @event = FactoryGirl.create(:event)
+  end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  describe "#create_from_event!(event)" do
+    it "should have an event with an id" do
+      @event.id.should_not be_nil
+    end
+
+    it "assigns a Standard Event name from the event JSON blob" do
+      name = @event.to_json['event']
+      name.should match 'viewed_landing'
+    end
+
+    it "assigns a Standard Event timestamp from the event JSON blob" do
+      timestamp = @event.to_json['timestamp']
+      timestamp.should match '2014-04-10T20:22:29.537Z'
     end
   end
 end
