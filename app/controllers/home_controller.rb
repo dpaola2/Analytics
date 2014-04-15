a6ba89f340ce83a705807a493f33011e907f6eda
Dave Paola <dpaola2@gmail.com>
2014-04-14 21:04:27 -0700

    fixed aggregation on homepage, added some averages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+  def index
+    hourly_groups = Event.group_by_hour(:created_at, :last => 24).count
+    amounts = hourly_groups.collect {|key, val| val }
+    @avg_per_hour = amounts.sum / amounts.count
+  end
 end
