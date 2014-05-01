57948d3d66e4f00a5daf85e31d88ffb5daf91476
Dave Paola <dpaola2@gmail.com>
2014-05-01 13:54:30 -0700

    remove event name from segment definitions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501204728) do
+ActiveRecord::Schema.define(version: 20140501205344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 20140501204728) do
 
   create_table "segment_definitions", force: true do |t|
     t.string   "name"
-    t.string   "event_name"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
