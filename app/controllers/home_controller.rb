1a909bae1ad0d8390df7a0354e0247392e8a786f
Dave Paola <dpaola2@gmail.com>
2014-05-06 13:11:48 -0700

    changed landing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
   def index
-    hourly_groups = Event.group_by_hour(:created_at, :last => 24).count
-    amounts = hourly_groups.collect {|key, val| val }
-    @avg_per_hour = amounts.sum / amounts.count
   end
 end
